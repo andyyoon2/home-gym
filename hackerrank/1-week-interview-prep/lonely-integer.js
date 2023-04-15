bd122691bc1986e2698d9d59c7dff1d006a74438
Andy Yoon <insoo.yoon@proton.me>
2023-04-15 12:25:30 -0700

    Lonely Integer

---
+++ hackerrank/1-week-interview-prep/lonely-integer.js
@@ -0,0 +1,67 @@
+// 2023-04-15
+// https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    // Populate hashmap O(n)
+    const seenValues = {};
+    for (let x of a) {
+        if (seenValues[x]) {
+            seenValues[x] += 1;
+        } else {
+            seenValues[x] = 1;
+        }
+    }
+    // Iterate hashmap and find the 1 value O(n)
+    for (let key in seenValues) {
+        if (seenValues[key] === 1) {
+            return key;
+        }
+    }
+    // Should never reach here
+    return 0;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+
