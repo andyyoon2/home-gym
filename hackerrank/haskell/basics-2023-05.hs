101163f317c30a9e43505c8dc526ab85eeeba122
Andy Yoon <insoo.yoon@proton.me>
2023-05-04 09:13:20 -0700

    Sum of odd elements

--- hackerrank/haskell/basics-2023-05.hs
+++ hackerrank/haskell/basics-2023-05.hs
@@ -4,3 +4,11 @@
 rev :: (Num a) => [a] -> [a]
 rev [] = []
 rev (x:xs) = rev xs ++ [x]
+
+-- Sum of odd elements
+-- https://www.hackerrank.com/challenges/fp-sum-of-odd-elements/problem
+f :: [Int] -> Int
+f [] = 0
+f (x:xs)
+    | x `mod` 2 == 1 = x + f xs
+    | otherwise      = f xs
