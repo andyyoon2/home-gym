a9475b752aab0a45afde0ce9974dd6cfa81f8441
Andy Yoon <insoo.yoon@proton.me>
2023-05-07 09:33:37 -0700

    Fibonacci

--- hackerrank/haskell/recursion-2023-05.hs
+++ hackerrank/haskell/recursion-2023-05.hs
@@ -5,3 +5,9 @@ gcd' n m
     | n == m = n
     | n > m  = gcd' (n-m) m
     | n < m  = gcd' (m-n) n
+
+-- https://www.hackerrank.com/challenges/functional-programming-warmups-in-recursion---fibonacci-numbers/problem
+fib 0 = error "Invalid input"
+fib 1 = 0
+fib 2 = 1
+fib n = fib (n-1) + fib (n-2)
