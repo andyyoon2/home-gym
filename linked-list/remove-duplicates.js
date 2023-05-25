a4caa7703f76124000e654bf44783b73d363cffe
Andy Yoon <insoo.yoon@proton.me>
2023-05-25 13:49:39 -0700

    Remove duplicates from LL

---
+++ linked-list/remove-duplicates.js
@@ -0,0 +1,24 @@
+// 2023-05-25
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/submissions/957326609/?envType=study-plan&id=data-structure-i
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let node = head;
+    while (node && node.next) {
+        if (node.val === node.next.val) {
+            node.next = node.next.next;
+        } else {
+            node = node.next;
+        }
+    }
+    return head;
+};
