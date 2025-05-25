a4288d8acbdb6befffb14c7def0a1f9d8b90829d
Andy Yoon <783285+andyyoon2@users.noreply.github.com>
2025-05-24 19:02:43 -0700

    fmt

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -2,37 +2,37 @@ use std::error::Error;
 use std::fs;
 
 pub struct Config {
-  pub query: String,
-  pub file_path: String,
+    pub query: String,
+    pub file_path: String,
 }
 
 impl Config {
-  // Unsure of why 'static is used, review lifetimes
-  pub fn build(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-          return Err("not enough arguments");
-      }
+    // Unsure of why 'static is used, review lifetimes
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-      // We will learn more efficient ways to do this, but for now we're calling
-      // clone so that we can return a new Config which owns its member Strings.
-      let query = args[1].clone();
-      let file_path = args[2].clone();
+        // We will learn more efficient ways to do this, but for now we're calling
+        // clone so that we can return a new Config which owns its member Strings.
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-      Ok(Config { query, file_path })
-  }
+        Ok(Config { query, file_path })
+    }
 }
 
 // Box<dyn Error> = The error will be some type that implements the Error trait
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  // Recall the ? returns the inner Result to the caller if it's an Err
-  let contents = fs::read_to_string(config.file_path)?;
+    // Recall the ? returns the inner Result to the caller if it's an Err
+    let contents = fs::read_to_string(config.file_path)?;
 
-  for line in search(&config.query, &contents) {
-    println!("{line}");
-  }
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
-  // Idiomatic way to return a Result of nothing from a fn
-  Ok(())
+    // Idiomatic way to return a Result of nothing from a fn
+    Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -48,7 +48,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn one_result() {
         let query = "duct";
@@ -56,7 +56,7 @@ mod tests {
 Rust:
 safe, fast, productive.
 Pick three.";
-        
+
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
     }
 }
